8f24ebb37bd5d948c8cdfbf4f2bc972f947b9f18
programmeur2 <apoirier@d-modules.com>
2017-03-06 16:12:01 -0500

    update

--- langevin_photo_api/apps/langevin_photo/models.py
+++ langevin_photo_api/apps/langevin_photo/models.py
@@ -13,8 +13,16 @@ class HomePage(Page):
     url_bouton_gauche = models.CharField(max_length=255)
     titre_bouton_droit = models.CharField(max_length=255, default="")
     url_bouton_droit = models.CharField(max_length=255)
+    photo_background = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
 
-    api_fields = ['titre', 'presentation', 'titre_bouton_gauche', 'url_bouton_gauche', 'titre_bouton_droit', 'url_bouton_droit']
+
+    api_fields = ['titre', 'presentation', 'titre_bouton_gauche', 'url_bouton_gauche', 'titre_bouton_droit', 'url_bouton_droit','photo_background','photo_background_url']
 
     content_panels = Page.content_panels + [
         FieldPanel('titre'),
@@ -23,4 +31,125 @@ class HomePage(Page):
         FieldPanel('url_bouton_gauche'),
         FieldPanel('titre_bouton_droit'),
         FieldPanel('url_bouton_droit'),
+        FieldPanel('photo_background'),
     ]
+    @property
+    def photo_background_url(self):
+        if self.photo_background:
+            return settings.MEDIA_URL + 'original_images/' + self.photo_background.filename
+
+
+class AlbumListPage(Page):
+    photo = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+    api_fields = ['photo','photo_url','categories']
+    
+    
+    subpage_types = ['langevin_photo.AlbumPage']
+
+    content_panels = Page.content_panels + [
+        FieldPanel('photo'),
+        InlinePanel('categories', label="Categories"),
+    ]
+
+    @property
+    def photo_url(self):
+        if self.photo:
+            return settings.MEDIA_URL + 'original_images/' + self.photo.filename
+
+class Categorie(Orderable):
+    page = ParentalKey(AlbumListPage, related_name='categories')
+    nom_categorie = models.CharField(max_length=50)
+    api_fields = ['nom_categorie']
+
+    panels = [
+        FieldPanel('nom_categorie'),
+    ]
+    def __unicode__(self):
+        return self.nom_categorie
+
+
+
+class AlbumPage(Page):
+    body = RichTextField(blank=True)
+    categorie = models.ForeignKey(Categorie, null=True, blank=True)
+    photo = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
+    api_fields = ['photos','body','photo','photo_url', 'categorie']
+    
+    parent_page_types = ['langevin_photo.AlbumListPage']
+    subpage_types = []
+
+    content_panels = Page.content_panels + [
+        FieldPanel('categorie'),
+        FieldPanel('body'),
+        FieldPanel('photo'),
+        InlinePanel('photos', label="Photos"),
+    ]
+
+    @property
+    def photo_url(self):
+        if self.photo:
+            return settings.MEDIA_URL + 'original_images/' + self.photo.filename
+
+
+class Photo(Orderable):
+    page = ParentalKey(AlbumPage, related_name='photos')
+    photo = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
+    api_fields = ['photo', 'photo_url']
+
+    panels = [
+        FieldPanel('photo'),
+    ]
+
+    @property
+    def photo_url(self):
+        if self.photo:
+            return settings.MEDIA_URL + 'original_images/' + self.photo.filename
+
+
+class AboutPage(Page):
+    body = RichTextField(blank=True)
+    name = models.CharField(max_length=100)
+    quote = models.CharField(max_length=255)
+    mr_title = models.CharField(max_length=150)
+    photo = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
+    api_fields = ['body','photo','photo_url','name','quote','mr_title']
+
+    content_panels = Page.content_panels + [
+        FieldPanel('name'),
+        FieldPanel('mr_title'),
+        FieldPanel('body'),
+        FieldPanel('quote'),
+        FieldPanel('photo'),
+    ]
+
+    @property
+    def photo_url(self):
+        if self.photo:
+            return settings.MEDIA_URL + 'original_images/' + self.photo.filename
--- stolen/assets/custom/myapp.js
+++ stolen/assets/custom/myapp.js
@@ -1,17 +1,114 @@
-$(document).ready(function () {
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+};
+
+
+
+function initAlbumList(){
+
+	
+	// all
+    $.getJSON("http://localhost:8000/api/v2/pages/10/?format=json", function (page_data) {
+        var template = $('#template-title').html();
+        var rendered = Mustache.render(template, page_data);
+        $('#templatetitle').html(rendered);
+        $('#backgroundimage').css("background-image", "url(http://localhost:8000"+page_data.photo_url+")");
+
+        //ajout filters menu
+		for (var car in page_data.categories){
+			var filter = $('<a href="#" data-filter=".'+page_data.categories[car].nom_categorie.toLowerCase()+'">'+page_data.categories[car].nom_categorie+'</a>');
+			filter.appendTo('#isotope-filter-collapse');
+		}
+	
+		//gets albums list
+    	$.getJSON("http://localhost:8000/api/v2/pages/?format=json", function (pages) {
+    		var albums = [];
+    		for (var key in pages.items){
+            	if (pages.items[key].meta.type == "langevin_photo.AlbumPage"){
+                	albums.push(pages.items[key].id);
+            	}
+        	}
+
+        	//render pictures list
+        	for (var ids in albums){
+            	$.getJSON("http://localhost:8000/api/v2/pages/"+albums[ids]+"/?format=json", function(page){
+                	var template = $('#template-albums-list').html();
+                	var rendered = Mustache.render(template, page);
+                	$('#albums-list').append(rendered);
+
+
+                	//link images
+                	for (var photo in page.photos){
+                		$('#photoid'+page.id).attr("src","http://localhost:8000"+page.photos[photo].photo_url);
+                		break;
+                	}
+
+                	//ajout categorie in album class
+                	for (var check in page_data.categories){
+                		if (page.categorie.id == page_data.categories[check].id){
+                			$('#item-album'+page.id).addClass(page_data.categories[check].nom_categorie.toLowerCase());
+                			break;
+                		}
+                	}
+            	});
+        	}
+    	});
+    });
+}
+
+
+function initIndex(){
     $.getJSON("http://localhost:8000/api/v2/pages/3/?format=json", function (page_data) {
         
-        console.log(page_data); 
         var template = $('#template-presentation').html();
         var rendered = Mustache.render(template, page_data);
-        console.log(rendered);
         $('#presentation').html(rendered);
+        $('#intro-section').css("background-image", "url(http://localhost:8000"+page_data.photo_background_url+")");
 
     });
 
-
-});
-
+}
 
 
+function initAbout(){
+    var aboutpageids = [];
+    var about = 0 ;
+    $.getJSON("http://localhost:8000/api/v2/pages/?format=json", function (data) {
+        
+        for (var key in data.items){
+            if (data.items[key].meta.type == "langevin_photo.AboutPage"){
+                aboutpageids.push(data.items[key].id);
+            }
+        }
+        aboutpageids.sort(function(a,b){
+            return b-a;
+        });
+        for (var id in aboutpageids){
+            about = aboutpageids[id];
+            break;
+        }
+
+        $.getJSON("http://localhost:8000/api/v2/pages/"+about+"/?format=json", function (page_data) {
+
+            var template = $('#template-about').html();
+            var rendered = Mustache.render(template, page_data);
+        
+            $('#about').html(rendered);
+            $('#abouttitle').text(page_data.title)
+
+        });
+
+    });
+}
 
